957bf63635fe0e5b81eb9044aaa0a0fc261028b8
kamalyakouty <61245578+kamalyakouty@users.noreply.github.com>
2020-02-19 12:37:39 -0500

    Update kamal-plugin.php

--- kamal-plugin.php
+++ kamal-plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: Kamal Plugin
  * Plugin URI: https://github.com/kamalyakouty/repo-test
  * Description: Hopefully it works
- * Version: 1.0.0.0
+ * Version: 1.1.0.0
  * Author: kamal yakouty
  * Author URI: http://wppusher.com
  * License: GNU GENERAL PUBLIC LICENSE
