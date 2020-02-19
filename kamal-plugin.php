1a75d71db2650ce9b4ac23b7b19776916bb734e8
kamalyakouty <61245578+kamalyakouty@users.noreply.github.com>
2020-02-19 12:34:34 -0500

    Create kamal-plugin.php

---
+++ kamal-plugin.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: Kamal Plugin
+ * Plugin URI: https://github.com/kamalyakouty/repo-test
+ * Description: Hopefully it works
+ * Version: 1.0.0.0
+ * Author: kamal yakouty
+ * Author URI: http://wppusher.com
+ * License: GNU GENERAL PUBLIC LICENSE
+ */
+
+// Add a change here
+
+// Add another change from the fork
+
+// Fix the bug
